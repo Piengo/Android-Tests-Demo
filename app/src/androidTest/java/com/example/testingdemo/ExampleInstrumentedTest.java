1eb8f3b83c0b4e628647b916f28de82e6355c4d5
Vuyo <hlumi68@gmail.com>
2021-04-06 09:49:03 +0200

    UI tests

--- app/src/androidTest/java/com/example/testingdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/testingdemo/ExampleInstrumentedTest.java
@@ -4,10 +4,19 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -24,3 +33,4 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.testingdemo", appContext.getPackageName());
     }
 }
+
--- app/src/main/java/com/example/testingdemo/MainActivity.java
+++ app/src/main/java/com/example/testingdemo/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.testingdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.inputField);
     }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+//            changes text
+            case R.id.changeText:
+                editText.setText("Lalala");
+                break;
+            case R.id.switchActivity:
+//                switches to the second activity
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("input", editText.getText().toString());
+                startActivity(intent);
+                break;
+        }
+    }
+
 }
